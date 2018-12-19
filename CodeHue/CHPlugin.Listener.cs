8d79b28104789d3d41fb5c76f602406ecb1e39ff
Appryl <cnl.maxime@gmail.com>
2018-12-19 13:28:45 +0100

    Updated code structure (again)

--- CodeHue/CHPlugin.Listener.cs
+++ CodeHue/CHPlugin.Listener.cs
@@ -1,4 +1,8 @@
+using System;
+using System.Collections.Generic;
 using System.Drawing;
+using System.IO;
+using System.Linq;
 using System.Threading.Tasks;
 using LyokoAPI.Events;
 using LyokoAPI.VirtualStructures;
@@ -6,84 +10,108 @@ using LyokoAPI.VirtualStructures.Interfaces;
 using Q42.HueApi;
 using Q42.HueApi.ColorConverters;
 using Q42.HueApi.ColorConverters.Original;
+using Q42.HueApi.Interfaces;
+using Q42.HueApi.Models.Bridge;
 
 namespace CodeHue
 {
-    public partial class CHPlugin
+    public class Listener
     {
-        public class Listener
+        private ILocalHueClient client;
+        private static bool _listening;
+
+        public Listener()
         {
-            private static bool _listening;
+            _listening = false;
+            BridgeConnection();
+        }
 
-            public Listener()
+        public void StartListening()
+        {
+            if (_listening) return;
+
+            TowerActivationEvent.Subscribe(OnTowerActivation);
+            TowerDeactivationEvent.Subscribe(OnTowerDeactivation);
+
+            _listening = true;
+        }
+
+        public void StopListening()
+        {
+            if (!_listening) return;
+
+            TowerActivationEvent.UnSubscribe(OnTowerActivation);
+            TowerDeactivationEvent.UnSubscribe(OnTowerDeactivation);
+        }
+
+        private async void OnTowerActivation(ITower tower)
+        {
+            Color color; //color class
+            switch (tower.Activator)
             {
-                _listening = false;
+                case APIActivator.XANA:
+                    color = Color.Red;
+                    break;
+                case APIActivator.JEREMIE:
+                    color = Color.Green;
+                    break;
+                case APIActivator.HOPPER:
+                    color = Color.White;
+                    break;
             }
 
-            public void StartListening()
-            {
-                if (_listening) 
-                {
-                    return;
-                }
-                
-                TowerActivationEvent.Subscribe(OnTowerActivation);
-                TowerDeactivationEvent.Subscribe(OnTowerDeactivation);
+            await SendCommand(color);
+        }
 
-                _listening = true;
+        private async void OnTowerDeactivation(ITower tower)
+        {
+            Color color;
+            color = Color.Black;
+            await SendCommand(color);
+        }
+        
+        public async Task BridgeConnection()
+        {
+            //Finding Bridge
+            IBridgeLocator locator = new HttpBridgeLocator();
+            IEnumerable<LocatedBridge> bridgeIPs = await locator.LocateBridgesAsync(TimeSpan.FromSeconds(5));
+            Console.WriteLine("Bridge located.");
+
+            //Checking Application Registering
+            client = new LocalHueClient(bridgeIPs.First().IpAddress);
+            string appKey = "";
+            var appKeyPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData),
+                "appKey.txt");
+            Console.WriteLine(appKeyPath);
+
+            if (System.IO.File.Exists(appKeyPath))
+            {
+                appKey = System.IO.File.ReadAllText(appKeyPath);
             }
 
-            public void StopListening()
+            if (appKey.Length == 0)
             {
-                if (!_listening)
-                {
-                    return;
-                }
-                
-                TowerActivationEvent.UnSubscribe(OnTowerActivation);
-                TowerDeactivationEvent.UnSubscribe(OnTowerDeactivation);
+                Console.WriteLine("Please press your Bridge's link button. Once done, press any key on your computer.");
+                Console.ReadKey();
+                appKey = await client.RegisterAsync("CodeHue", "Computer");
+                System.IO.File.WriteAllText(appKeyPath, appKey);
+                Console.WriteLine("Key registered.");
             }
 
-            private async void OnTowerActivation(ITower tower)
-            {
-                Color color; //color class
-                switch (tower.Activator)
-                {
-                    case APIActivator.XANA:
-                        color = Color.Red;
-                        break;
-                    case APIActivator.JEREMIE:
-                        color = Color.Green;
-                        break;
-                    case APIActivator.HOPPER:
-                        color = Color.White;
-                        break;
-                }
+            //Connecting To Bridge
+            client.Initialize(appKey);
+            Console.WriteLine("Successfully connected to the Bridge.");
+        }
 
-                await SendCommand(color);
-            }
-            
-            private async void OnTowerDeactivation(ITower tower)
-            {
-                Color color;
-                color = Color.Black;
-                await SendCommand(color);
-            }
-            
-            public async Task SendCommand(Color color)
-            {
-                var command = new LightCommand();
-                if (color == Color.Black)
-                {
-                    command.TurnOff();
-                }
-                else
-                {
-                    command.TurnOn().SetColor(new RGBColor(color.ToString()));
-                }
+        public async Task SendCommand(Color color)
+        {
+            var command = new LightCommand();
+            if (color == Color.Black)
+                command.TurnOff();
+            else
+                command.TurnOn().SetColor(new RGBColor(color.ToString()));
 
-                await client.SendCommandAsync(command);
-            }
+            await client.SendCommandAsync(command);
         }
     }
 }
--- CodeHue/CHPlugin.cs
+++ CodeHue/CHPlugin.cs
@@ -1,32 +1,16 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Drawing;
-using System.IO;
-using System.Linq;
-using System.Media;
-using System.Net;
-using System.Net.Configuration;
-using System.Threading.Tasks;
 using LyokoAPI.Plugin;
-using Q42.HueApi;
-using Q42.HueApi.ColorConverters;
-using Q42.HueApi.ColorConverters.Original;
-using Q42.HueApi.Interfaces;
-using Q42.HueApi.Models.Bridge;
-using static System.String;
 
 namespace CodeHue
 {
-    public partial class CHPlugin : LyokoAPIPlugin
+    public class CHPlugin : LyokoAPIPlugin
     {
         public override string Name { get; } = "CHPlugin";
         public override string Author { get; } = "Appryl";
-        private ILocalHueClient client;
 
         protected override bool OnEnable()
         {
             Console.WriteLine("Launching CodeHue...");
-            BridgeConnection();
             Listener hueListener = new Listener();
         }
 
@@ -41,39 +25,6 @@ namespace CodeHue
 
         public override void OnGameEnd(bool failed)
         {
-        }
-
-        public async Task BridgeConnection()
-        {
-            //Finding Bridge
-            IBridgeLocator locator = new HttpBridgeLocator();
-            IEnumerable<LocatedBridge> bridgeIPs = await locator.LocateBridgesAsync(TimeSpan.FromSeconds(5));
-            Console.WriteLine("Bridge located.");
-
-            //Checking Application Registering
-            client = new LocalHueClient(bridgeIPs.First().IpAddress);
-            string appKey = "";
-            var appKeyPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData),
-                "appKey.txt");
-            Console.WriteLine(appKeyPath);
-
-            if (System.IO.File.Exists(appKeyPath))
-            {
-                appKey = System.IO.File.ReadAllText(appKeyPath);
-            }
-
-            if (appKey.Length == 0)
-            {
-                Console.WriteLine("Please press your Bridge's link button. Once done, press any key on your computer.");
-                Console.ReadKey();
-                appKey = await client.RegisterAsync("CodeHue", "Computer");
-                System.IO.File.WriteAllText(appKeyPath, appKey);
-                Console.WriteLine("Key registered.");
-            }
-
-            //Connecting To Bridge
-            client.Initialize(appKey);
-            Console.WriteLine("Successfully connected to the Bridge.");
         }
     }
 }
